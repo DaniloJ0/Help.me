4b448135cd6277322d22c6beb991b6451cd1d7c6
Danilo Jimenez Rangel <rangeljd@uninorte.edu.co>
2022-03-28 20:57:01 -0500

    diseño de las ventanas de menu y ayuda

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
+import 'package:apphelpme/ui/help_page.dart';
 import 'package:flutter/material.dart';
 
+import 'ui/menu_page.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Base project',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: SafeArea(child: MenuPage()),
+        ));
+  }
+}
 //Danilo
