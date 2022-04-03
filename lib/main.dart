6261fad429cb1d64e7aa4f3f35bd38fe58394f68
camailo19 <84298966+camailo19@users.noreply.github.com>
2022-04-03 14:46:46 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,20 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Base project',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: SafeArea(child: MenuPage()),
+        ));
+  }
+}
