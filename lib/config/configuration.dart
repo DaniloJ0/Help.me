e9125e567d30ac7848404346f7daeec7e40c77d6
vrmedina <vrmm1998@gmail.com>
2022-04-26 09:47:24 -0500

    added configuration.dart and json

---
+++ lib/config/configuration.dart
@@ -0,0 +1,12 @@
+class Configuration {
+  static const apiKey = "AIzaSyDcYemfpkSxEF7JMCKx28SzCgqQHd23SYs";
+  static const authDomain = "help-me-bc913.firebaseapp.com";
+  static const databaseURL =
+      "https://help-me-bc913-default-rtdb.firebaseio.com/";
+  static const projectId = "help-me-bc913";
+  static const storageBucket = "help-me-bc913.appspot.com";
+  static const appId = "1:419964649158:android:81159f7786526fde4661c7";
+
+  static const messagingSenderId = "";
+  static const measurementId = "";
+}
