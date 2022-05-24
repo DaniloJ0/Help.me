d789be6f7d6e20e5d9bab8e6be8cde6a75e744bb
vrmedina <vrmm1998@gmail.com>
2022-05-23 22:25:00 -0500

    Improved contacts view to work with sharedprefs

--- lib/ui/contacts_page.dart
+++ lib/ui/contacts_page.dart
@@ -1,123 +1,81 @@
-import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:flutter_contacts/flutter_contacts.dart';
-import 'dart:typed_data';
-import '../../domain/models/human.dart';
+import 'package:fluttercontactpicker/fluttercontactpicker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Contacts extends StatefulWidget {
-  Contacts({Key? key}) : super(key: key);
   @override
   _ContactsState createState() => _ContactsState();
 }
 
 class _ContactsState extends State<Contacts> {
-List<Contact> _suggestions = <Contact>[];
-  Set<Contact> _saved = <Contact>{};
-  List<Contact>? contacts;
-  @override
-  void initState() {
-    super.initState();
-    getContact();
-    _suggestions = <Contact>[];
-    _saved = <Contact>{};
+  List<String> contactsNums = [];
+  List<String> contactsNames = [];
+
+  late SharedPreferences prefs;
+
+  _initializeContacts() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      contactsNums = prefs.getStringList('nums') ?? [];
+      contactsNames = prefs.getStringList('names') ?? [];
+    });
   }
 
-  void getContact() async {
-    if (await FlutterContacts.requestPermission()) {
-      contacts = await FlutterContacts.getContacts(
-          withProperties: true, withPhoto: true);
-      // print(contacts);
-      setState(() {});
-    }
+  @override
+  void initState() {
+    _initializeContacts();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Contactos de Emergencia",
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.list),
-              onPressed: _pushSaved,
-              tooltip: 'Contactos Seleccionados',
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        title: Text("Contactos de Emergencia"),
+      ),
+      body: contactsNames.length > 0
+          ? ListView.builder(
+              itemCount: contactsNames.length,
+              itemBuilder: (BuildContext ctx, int i) {
+                return Card(
+                  margin: EdgeInsets.all(5),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.account_circle,
+                      size: 35,
+                    ),
+                    title: Text(contactsNames[i]),
+                    subtitle: Text(contactsNums[i]),
+                    trailing: GestureDetector(
+                      child: Icon(Icons.delete),
+                      onTap: () async {
+                        setState(() {
+                          contactsNames.removeAt(i);
+                          contactsNums.removeAt(i);
+                        });
+                        await prefs.setStringList('nums', contactsNums);
+                        await prefs.setStringList('names', contactsNames);
+                      },
+                    ),
+                  ),
+                );
+              },
+            )
+          : Center(
+              child: Text('Aún no has seleccionado contactos'),
             ),
-          ],
-        ),
-        body: (contacts) == null
-            ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                padding: const EdgeInsets.all(10.0),
-                itemCount: contacts!.length,
-                itemBuilder: (BuildContext context, int index) {
-                  Uint8List? image = contacts![index].photo;
-                  String num = (contacts![index].phones.isNotEmpty)
-                      ? (contacts![index].phones.first.number)
-                      : "--";
-                  final alreadySaved = _saved.contains(contacts![index]);
-                  return ListTile(
-                      leading: (contacts![index].photo == null)
-                          ? const CircleAvatar(child: Icon(Icons.person))
-                          : CircleAvatar(backgroundImage: MemoryImage(image!)),
-                      title: Text(
-                          "${contacts![index].name.first} ${contacts![index].name.last}"),
-                      subtitle: Text(num),
-                      trailing: Icon(
-                        alreadySaved ? Icons.favorite : Icons.favorite_border,
-                        color: alreadySaved ? Colors.red : null,
-                        semanticLabel:
-                            alreadySaved ? 'Quitar Selección' : 'Seleccionar',
-                      ),
-                      onTap: () {
-                        if (contacts![index].phones.isNotEmpty) {
-                          setState(() {
-                            if (alreadySaved) {
-                              _saved.remove(contacts![index]);
-                            } else {
-                              _saved.add(contacts![index]);
-                            }
-                          });
-                        }
-                      });
-                },
-              ));
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final tiles = _saved.map(
-            (contacts) {
-              String num = contacts.phones.first.number;
-              return ListTile(
-                /* title: Text(
-                  contacts.toString(),*/
-                leading: (contacts.photo == null)
-                    ? const CircleAvatar(child: Icon(Icons.person))
-                    : const CircleAvatar(child: Icon(Icons.person)),
-                title: Text("${contacts.name.first} ${contacts.name.last}"),
-                subtitle: Text(num),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(
-                  context: context,
-                  tiles: tiles,
-                ).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Contactos Seleccionados'),
-            ),
-            body: ListView(children: divided),
-          );
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          final PhoneContact contact =
+              await FlutterContactPicker.pickPhoneContact();
+          setState(() {
+            contactsNames.add(contact.fullName.toString());
+            contactsNums.add(contact.phoneNumber!.number.toString());
+          });
+          await prefs.setStringList('nums', contactsNums);
+          await prefs.setStringList('names', contactsNames);
         },
       ),
     );
